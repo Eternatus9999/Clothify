5ac4a16d0e1ac7386ca2e7bbbb26d10b0515b2bf
Eternatus9999 <eternatus9999@gmail.com>
2024-10-11 05:47:43 +0530

    Add some attributes to the User dto class

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -1,5 +1,21 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
+
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String password;
 
 }
